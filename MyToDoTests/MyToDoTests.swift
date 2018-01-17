63dbbd839d83879a03b03890996bad73883a2a7e
angiemugo <mugoangela@gmail..com>
2018-01-17 09:14:37 +0300

    Initial Commit

---
+++ MyToDoTests/MyToDoTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyToDoTests.swift
+//  MyToDoTests
+//
+//  Created by Angie Mugo on 17/01/2018.
+//  Copyright © 2018 Angie Mugo. All rights reserved.
+//
+
+import XCTest
+@testable import MyToDo
+
+class MyToDoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
